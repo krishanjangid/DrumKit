1c1a9862ddf4ac47994eb7e3152124efd66ed1f0
krishanjangid <krishanjangid516@gmail.com>
2023-01-27 22:30:39 +0530

    key press updated

--- index.js
+++ index.js
@@ -2,36 +2,45 @@ for(i=0;i<document.querySelectorAll('button').length;i++)
 {
 document.querySelectorAll('button')[i].addEventListener("click",function() {
   var btn = this.innerHTML;
-    switch(btn)
-    {
-      case 'w':
-        var audio = new Audio('sounds/tom-1.mp3');
-        audio.play();
-        break;
-      case 'a':
-        var audio = new Audio('sounds/tom-2.mp3');
-        audio.play();
-        break;
-      case 's':
-        var audio = new Audio('sounds/tom-3.mp3');
-        audio.play();
-        break;
-      case 'd':
-        var audio = new Audio('sounds/tom-4.mp3');
-        audio.play();
-        break;
-      case 'j':
-        var audio = new Audio('sounds/crash.mp3');
-        audio.play();
-        break;
-      case 'k':
-        var audio = new Audio('sounds/kick-bass.mp3');
-        audio.play();
-        break;
-      case 'l':
-        var audio = new Audio('sounds/snare.mp3');
-        audio.play();
-        break;
-    }
+  keypressed(btn);
 });
+}
+
+document.addEventListener("keypress",function () {
+  keypressed(event.key);
+});
+
+
+function keypressed(key) {
+  switch(key)
+  {
+    case 'w':
+      var audio = new Audio('sounds/tom-1.mp3');
+      audio.play();
+      break;
+    case 'a':
+      var audio = new Audio('sounds/tom-2.mp3');
+      audio.play();
+      break;
+    case 's':
+      var audio = new Audio('sounds/tom-3.mp3');
+      audio.play();
+      break;
+    case 'd':
+      var audio = new Audio('sounds/tom-4.mp3');
+      audio.play();
+      break;
+    case 'j':
+      var audio = new Audio('sounds/crash.mp3');
+      audio.play();
+      break;
+    case 'k':
+      var audio = new Audio('sounds/kick-bass.mp3');
+      audio.play();
+      break;
+    case 'l':
+      var audio = new Audio('sounds/snare.mp3');
+      audio.play();
+      break;
+  }
 }
